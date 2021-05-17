0253e7f60a4ac85eb908c3d9597f30f68b754638
Devan <devan.madlani@klm.com>
2021-05-17 23:18:00 +0530

    update automation

--- cypress/integration/event-overview.spec.js
+++ cypress/integration/event-overview.spec.js
@@ -11,4 +11,24 @@ describe('Event overview component', ()=> {
     it('should navigate to form subscription page', function () {
         cy.url().should('include', 'subscription-form/MAR2021032510');
     });
+
+    it('should fill all the form details', function () {
+        cy.get('#name').clear().type('Devan');
+        cy.get('#email').clear().type('madlanidevan@gmail.com');
+        cy.get('#date-picker').clear().type('1993-02-24');
+        cy.get('[type="radio"]').first().check({ force: true })
+        cy.get('select').select('Amsterdam').should('have.value', '3: Amsterdam')
+    });
+
+    it('should successfully submit the form ', function () {
+        cy.get('#submit-form').click();
+    });
+
+    it('should click on view events subscribed', function () {
+        cy.get('#event-subscribed-btn').click();
+    });
+
+    it('should unsubscribe event on click of delete button', function () {
+        cy.get('#delete-event').click();
+    });
 });
